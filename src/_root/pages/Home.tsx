83fab612fc679a95d5af13131a7a797d52069c00
PedroReoli <pedrolucas2012@gmail.com>
2024-01-23 16:16:22 -0300

    Beg Home
    tentando fazer um fundo animado que reconhece o mouse

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -2,7 +2,17 @@
 
 const Home = () => {
   return (
-    <div>Hossadsasdasdme</div>
+    <div className="home-text-container">
+        <div>
+          <h2 className="h3-home">Olá,eu sou o </h2>
+          <h1 className="h1-home">Pedro Reis</h1>
+          <h2 className="h2-home">Web Dev</h2>
+        </div>
+        <div className="container-button">
+          <button className="hire-button">Me contrate</button>
+          <button className="learn-more-button">Saiba mais</button>
+        </div>
+    </div>
   )
 }
 
