fa6902ca35900d6be183957d461152e3090d0e7f
PedroReoli <pedrolucas2012@gmail.com>
2024-01-27 18:16:50 -0300

    Almost done Portfolio
    Depois tenho que so acrescentar os projetos na mão

--- src/_root/pages/Portfolio.tsx
+++ src/_root/pages/Portfolio.tsx
@@ -1,9 +1,43 @@
 
-
 const Portfolio = () => {
+  const projects = [
+    {
+      title: 'Projeto 1',
+      description: 'Descrição do Projeto 1...',
+      technologies: ['React', 'Node.js'],
+      image: 'public/images/project1.jpg',
+      link: 'https://example.com/project1',
+    },
+    {
+      title: 'Projeto 2',
+      description: 'Descrição do Projeto 2...',
+      technologies: ['HTML', 'CSS', 'JavaScript'],
+      image: 'public/images/project2.jpg',
+      link: 'https://example.com/project2',
+    },
+    // Adicione mais projetos conforme necessário
+  ];
+  
   return (
-    <div>Portfolio</div>
-  )
-}
+    <div className="portfolio-container">
+      <h2 className="h2-h">Portfolio</h2>
+      <div className="projects-grid">
+        {projects.map((project, index) => (
+          <div key={index} className="project-card">
+            <div className="project-details">
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+              <p>Tecnologias: {project.technologies.join(', ')}</p>
+              <a href={project.link} target="_blank" rel="noopener noreferrer">
+                Ver Projeto
+              </a>
+            </div>
+            <img src={project.image} alt={project.title} className="project-image" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
